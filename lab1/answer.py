99a73bd74f11ce7539e54821f18f07ccd7f79cd0
Magnus Andersson <bredsjomagnus@yahoo.se>
2018-12-11 10:50:23 +0100

    Adding labbs

---
+++ lab1/answer.py
@@ -0,0 +1,569 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+7cf244cfbcc55a41070a891838e397b4
+python
+Elevensval - Labb 1
+Magnus Andersson
+2018-11-22 09:21:27
+
+Generated 2016-01-24 10:21:28 by dbwebb lab-utility v2.0.1x (2015-09-29).
+https://github.com/mosbth/lab
+"""
+
+from Dbwebb import Dbwebb
+
+dbwebb = Dbwebb()
+print("Ready to begin.")
+
+
+"""
+==========================================================================
+Lab 1 - python
+
+If you need to peek at examples or just want to know more, take a look at
+the page: https://docs.python.org/3/library/index.html. Here you will find
+everything this lab will go through and much more.
+"""
+
+"""
+--------------------------------------------------------------------------
+Section 1. Integers, strings, floats and basic arithmetics
+
+The foundation of numbers and basic arithmetic.
+"""
+
+"""
+Exercise 1.1
+
+Create a variable called 'numOne' and give it the value 76. Create another
+variable called 'numTwo' and give it the value 76. Create a third variable
+called 'result' and assign to it the sum of the first two variables. Answer
+with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.1", ANSWER, False))
+
+"""
+Exercise 1.2
+
+Create a variable called 'numThree' and give it the value 81. Create
+another variable called 'numFour' and give it the value 61. Subtract
+'numThree' from 'numFour' and answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.2", ANSWER, False))
+
+"""
+Exercise 1.3
+
+Find out the product of 'numOne' and 'numThree' and answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.3", ANSWER, False))
+
+"""
+Exercise 1.4
+
+Divide 30 with 5 and answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.4", ANSWER, False))
+
+"""
+Exercise 1.5
+
+Create a variable called 'floatOne' and give it the value 98.76. Create
+another variable called 'floatTwo' and give it the value 21.93. Sum the two
+values and answer with the result, rounded to 2 decimals.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.5", ANSWER, False))
+
+"""
+Exercise 1.6
+
+Subtract 'floatTwo' from 'floatOne' and answer with the result. Round to 2
+decimals.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+floatOne = 98.76
+floatTwo = 21.93
+result = floatOne - floatTwo
+
+result = round(result, 2)
+
+ANSWER = result
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.6", ANSWER, False))
+
+"""
+Exercise 1.7
+
+Answer with the product of 'floatOne' and 'floatTwo', rounded to 4
+decimals.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.7", ANSWER, False))
+
+"""
+Exercise 1.8
+
+Create three variables: 'n1' = 327, 'n2' = 209 and 'n3' = 80. Sum up 'n1'
+and 'n2'. Subtract 'n3' and answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.8", ANSWER, False))
+
+"""
+Exercise 1.9
+
+Answer with the result of 507 modulo (%) 35.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+result = 507 % 35
+
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.9", ANSWER, False))
+
+"""
+Exercise 1.10
+
+Add the word: 'device' to the word: 'beach' and answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+word1 = 'beach'
+word2 = 'device'
+result = word1 + word2
+
+
+ANSWER = result
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.10", ANSWER, False))
+
+"""
+--------------------------------------------------------------------------
+Section 2. Conditions, exceptions, booleans, if, else and elif
+
+
+"""
+
+"""
+Exercise 2.1
+
+Answer with the boolean value of: 70 is less than 259.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+result = 70 < 259
+ANSWER = result
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.1", ANSWER, False))
+
+"""
+Exercise 2.2
+
+Answer with the boolean value of: 444 is larger than 490.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.2", ANSWER, False))
+
+"""
+Exercise 2.3
+
+Answer with the boolean value of: 70 == 444.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+result = 70 == 444
+ANSWER = result
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.3", ANSWER, False))
+
+"""
+Exercise 2.4
+
+Create three variables representing dice values: 'd1' = 1, 'd2' = 6 and
+'d3' = 6. Sum them up and answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.4", ANSWER, False))
+
+"""
+Exercise 2.5
+
+Create an if statement to see if the total value of your dices is 11 or
+higher. If that is the case, answer with the string: 'big', else answer
+with the string: 'nothing'.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+if result >= 11:
+    dices = 'big'
+else:
+    dices = 'nothing'
+
+ANSWER = dices
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.5", ANSWER, False))
+
+"""
+Exercise 2.6
+
+Create an elif statement that checks your total dice value. The checks and
+results should be: three of the same value = 'triple', total of 11 or
+higher = 'big', total of 10 or lower = 'small'. If you get a triple you
+should not make more checks.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+d1 = 1
+d2 = 6
+d3 = 6
+result = d1 + d2 + d3
+
+if d1 == d2 == d3:
+    dices = 'triple'
+elif result >= 11:
+    dices = 'big'
+else: 
+    dices = 'small'
+
+ANSWER = dices
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.6", ANSWER, False))
+
+"""
+--------------------------------------------------------------------------
+Section 3. Built-in functions
+
+Some useful built-in functions
+"""
+
+"""
+Exercise 3.1
+
+Use max() to find the largest number in the serie:
+6,8,95,2,12,152,4,78,621,45. Answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+serie = (6,8,95,2,12,152,4,78,621,45)
+print(type(serie))
+
+maximum = max(serie)
+
+ANSWER = maximum
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("3.1", ANSWER, False))
+
+"""
+Exercise 3.2
+
+Use min() to find the smallest number in the serie:
+6,8,95,2,12,152,4,78,621,45. Answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("3.2", ANSWER, False))
+
+"""
+Exercise 3.3
+
+Use len() to find the number of letters in the word: error. Answer with the
+result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+word1 = 'error'
+count = len(word1)
+
+ANSWER = count
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("3.3", ANSWER, False))
+
+"""
+Exercise 3.4
+
+Convert the number 353 to a string and add it to the word 'error'. Answer
+with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+number = 353
+stringnumber = str(number)
+
+result = 'error' + stringnumber
+
+
+ANSWER = result
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("3.4", ANSWER, False))
+
+"""
+Exercise 3.5
+
+Convert the number 640.49 to an integer and then to a string. Add it to the
+beginning of the word: 'error'. Answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+number = 640.49
+intnumber = int(number)
+stringnumber = str(intnumber)
+
+result = stringnumber + 'error'
+ANSWER = result
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("3.5", ANSWER, False))
+
+"""
+--------------------------------------------------------------------------
+Section 4. Functions
+
+Basic functions
+"""
+
+"""
+Exercise 4.1
+
+Create a function called 'prodNr' that takes two arguments, 34 and 77. The
+function should return the product of the numbers. Answer with a call to
+the function.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("4.1", ANSWER, False))
+
+"""
+Exercise 4.2
+
+Create a function called 'funnyWord' that takes one argument and adds it to
+the string ' is a funny word'. If the argument is 'water', the function
+should return: 'water is a funny word'. Use the argument 'restaurant' and
+answer with a call to the function.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("4.2", ANSWER, False))
+
+"""
+Exercise 4.3
+
+Create a function called 'inRange' that takes one argument. The function
+should return 'true' if the argument is higher than 50 and lower than 100.
+If the number is out of range, the function should return 'false'. The
+return type should be boolean. Use the argument 31 and answer with a call
+to the function.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("4.3", ANSWER, False))
+
+"""
+--------------------------------------------------------------------------
+Section 5. Iteration and loops
+
+For-loops and while-loops.
+"""
+
+"""
+Exercise 5.1
+
+Create a while-loop that adds 8 to the number 64, 73 times. Answer with the
+result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("5.1", ANSWER, False))
+
+"""
+Exercise 5.2
+
+Create a while-loop that subtracts 5 from 28, 68 times. Answer with the
+result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("5.2", ANSWER, False))
+
+"""
+Exercise 5.3
+
+Create a for-loop that counts the number of elements in the serie:
+6,8,95,2,12,152,4,78,621,45. Answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("5.3", ANSWER, False))
+
+"""
+Exercise 5.4
+
+Create a for-loop that sums up the numbers in the serie:
+67,2,12,28,128,15,90,4,579,450. Answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("5.4", ANSWER, False))
+
+"""
+Exercise 5.5
+
+Create a for-loop that finds the largest number in the serie:
+6,8,95,2,12,152,4,78,621,45. Answer with the result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("5.5", ANSWER, False))
+
+"""
+Exercise 5.6
+
+Create a for-loop that goes through the numbers:
+67,2,12,28,128,15,90,4,579,450. If the current number is even, you should
+add it to a variable and if the current number is odd, you should subtract
+it from the variable. Answer with the final result.
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("5.6", ANSWER, False))
+
+
+dbwebb.exitWithSummary()
---
+++ lab2/answer.py
@@ -0,0 +1,337 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ae949ef06c305dd1bd7f4af13bf76a11
+python
+lab2
+maaa16
+2016-01-29 05:52:53
+v2.0.1x (2015-09-29)
+
+Generated 2016-01-29 06:52:54 by dbwebb lab-utility v2.0.1x (2015-09-29).
+https://github.com/mosbth/lab
+"""
+
+from Dbwebb import Dbwebb
+
+dbwebb = Dbwebb()
+print("Ready to begin.")
+
+
+"""
+==========================================================================
+Lab 2 - python 
+ 
+Strings and files 
+"""
+
+"""
+--------------------------------------------------------------------------
+Section 1. Strings 
+ 
+The basics of strings 
+"""
+
+"""
+Exercise 1.1 
+ 
+Assign the word: 'lemonade' to a variable and put your variable as the
+answer. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.1", ANSWER, False))
+
+"""
+Exercise 1.2 
+ 
+Assign the word 'lemonade' to a variable. Create another variable where you
+put the first and the last letter in the word. Answer with the second
+variable. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.2", ANSWER, False))
+
+"""
+Exercise 1.3 
+ 
+Assign the word: melon to a variable. Answer with the length of the word as
+an integer. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.3", ANSWER, False))
+
+"""
+Exercise 1.4 
+ 
+Use the 'in-operator' to see if the word: 'train' contains the letter 'a'.
+Answer with a boolean result. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.4", ANSWER, False))
+
+"""
+Exercise 1.5 
+ 
+Make all the letters in the word 'melon' capitalized. Answer with the
+capitalized word. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+    
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.5", ANSWER, False))
+
+"""
+Exercise 1.6 
+ 
+Use the built-in function 'startswith()' to see if the word 'lemonade'
+starts with the letter 'e'. Answer with the boolean value. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.6", ANSWER, False))
+
+"""
+Exercise 1.7 
+ 
+Assign the words: 'lemonade' and 'onion' to two different variables. Create
+a function and pass the two words as arguments to it. Your function should
+return them as a single word. Answer with the result. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.7", ANSWER, False))
+
+"""
+Exercise 1.8 
+ 
+Create a function and pass the word: 'lemonade' to it. Your function should
+return the sentence: 'This word was: lemonade'. Answer with the result. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.8", ANSWER, False))
+
+"""
+Exercise 1.9 
+ 
+Create a function and pass the word: 'onion' to it. Your function should
+return the string 'yes' if the word is longer than 5 characters. Else
+return 'no'. Answer with the result. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.9", ANSWER, False))
+
+"""
+Exercise 1.10 
+ 
+Create a function and pass the word: 'melon' to it. Your function should
+return a string with the word backwards. Answer with the result. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.10", ANSWER, False))
+
+"""
+Exercise 1.11 
+ 
+Create a function and pass the word: 'lemonade' to it. Your function should
+exclude the first and the last letter of the word and return it. Answer
+with the result. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.11", ANSWER, False))
+
+"""
+Exercise 1.12 
+ 
+Use str.format() to print out: 'My 'string' has 'integer' 'string''. Use
+the values: 'brother', '2' and 'dogs'. Answer with the result. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.12", ANSWER, False))
+
+"""
+Exercise 1.13 
+ 
+Use 'find' and 'slice' on the string: '154.84.56.0 : (wallpaper), soda' to
+get the word inside the parenthesis. Answer with the word as a string. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("1.13", ANSWER, False))
+
+"""
+--------------------------------------------------------------------------
+Section 2. Files 
+ 
+This section uses the text file: 'httpd-access.txt' 
+"""
+
+"""
+Exercise 2.1 
+ 
+Open the file and count the number of times a line starts with '81'. Answer
+with the result as an integer. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.1", ANSWER, False))
+
+"""
+Exercise 2.2 
+ 
+Find out the last 4 digits on line 821 in the file. Answer with the result
+as an integer. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.2", ANSWER, False))
+
+"""
+Exercise 2.3 
+ 
+Find out which of the ip adresses 81.226.253.26 and 95.19.133.73 that has
+the highest amount of entries in the file. Answer with the result as an
+integer. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.3", ANSWER, False))
+
+"""
+Exercise 2.4 
+ 
+Count the number of periods (.) there are in the file. Use the built-in
+function count() on the file after you have converted it to a string.
+Answer with the result as an integer. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.4", ANSWER, False))
+
+"""
+Exercise 2.5 
+ 
+Find the characters on line 637 from index 65 to index 75. Make sure that
+the character at index 75 also gets included. Answer with the piece of
+string you found. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.5", ANSWER, False))
+
+"""
+Exercise 2.6 
+ 
+Find the last element (digit) on each line, if there are any, and sum all
+that are even. Answer with the result as an integer. 
+
+Write your code below and put the answer into the variable ANSWER.
+"""
+
+ANSWER = "Replace this text with the variable holding the answer."
+
+# Is the answer as expected?
+# When you get stuck - change False to True to get a hint.
+print(dbwebb.assertEqual("2.6", ANSWER, False))
+
+
+dbwebb.exitWithSummary()
